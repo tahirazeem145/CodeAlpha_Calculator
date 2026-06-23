ce679e5ccab9e40727a98292521bd2645b8e16d4
tahirazeem145 <tahirazeems145s@gmail.com>
2026-06-23 23:54:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function displayvalue(value){
+   document.getElementById("display").value = document.getElementById("display").value + value
+}
+function cleardisplay(){
+    document.getElementById("display").value=""
+}
+function calculateval(){
+    var userinput=document.getElementById("display").value
+    var result=eval(userinput)
+    document.getElementById("display").value=result
+}
